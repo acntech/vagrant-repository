23c341f12998a393b85ee4e5c1e34396bcc13cef
Teo <onlyteo@live.com>
2018-12-03 12:13:00 +0100

    Added option to set host for box download URI to handle proxy setups

--- api/common/src/main/java/no/acntech/common/config/ApplicationProperties.java
+++ api/common/src/main/java/no/acntech/common/config/ApplicationProperties.java
@@ -1,11 +1,12 @@
 package no.acntech.common.config;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "acntech")
@@ -13,7 +14,29 @@ public class ApplicationProperties {
 
     @NotNull
     @Valid
+    private Api api = new Api();
+    @NotNull
+    @Valid
+    private Proxy proxy = new Proxy();
+    @NotNull
+    @Valid
     private File file = new File();
+
+    public Api getApi() {
+        return api;
+    }
+
+    public void setApi(Api api) {
+        this.api = api;
+    }
+
+    public Proxy getProxy() {
+        return proxy;
+    }
+
+    public void setProxy(Proxy proxy) {
+        this.proxy = proxy;
+    }
 
     public File getFile() {
         return file;
@@ -24,14 +47,63 @@ public class ApplicationProperties {
     }
 
     @Validated
+    public static class Api {
+
+        @NotBlank
+        private String clientContextPath;
+
+        public String getClientContextPath() {
+            return clientContextPath;
+        }
+
+        public void setClientContextPath(String clientContextPath) {
+            this.clientContextPath = clientContextPath;
+        }
+    }
+
+    @Validated
+    public static class Proxy {
+
+        private String scheme = "http";
+        private String host;
+        private Integer port;
+
+        public String getScheme() {
+            return scheme;
+        }
+
+        public void setScheme(String scheme) {
+            this.scheme = scheme;
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public void setHost(String host) {
+            this.host = host;
+        }
+
+        public Integer getPort() {
+            return port;
+        }
+
+        public void setPort(Integer port) {
+            this.port = port;
+        }
+
+        public boolean isSet() {
+            return StringUtils.isNoneBlank(scheme) && StringUtils.isNoneBlank(host) && port != null;
+        }
+    }
+
+    @Validated
     public static class File {
 
         @NotBlank
         private String uploadDir;
         @NotBlank
         private String defaultFileName;
-        @NotBlank
-        private String rootContextPath;
 
         public String getUploadDir() {
             return uploadDir;
@@ -47,14 +119,6 @@ public class ApplicationProperties {
 
         public void setDefaultFileName(String defaultFileName) {
             this.defaultFileName = defaultFileName;
-        }
-
-        public String getRootContextPath() {
-            return rootContextPath;
-        }
-
-        public void setRootContextPath(String rootContextPath) {
-            this.rootContextPath = rootContextPath;
         }
     }
 }
